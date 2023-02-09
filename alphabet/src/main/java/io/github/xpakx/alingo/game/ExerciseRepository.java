058e641b47d0c4a49ed59c810bd290b129b02fd3
xpakx <55586470+xpakx@users.noreply.github.com>
2023-02-09 14:35:09 +0100

    feat(alphabet): cache answers for exercises

--- alphabet/src/main/java/io/github/xpakx/alingo/game/ExerciseRepository.java
+++ alphabet/src/main/java/io/github/xpakx/alingo/game/ExerciseRepository.java
@@ -1,10 +1,13 @@
 package io.github.xpakx.alingo.game;
 
 import io.github.xpakx.alingo.game.dto.ExerciseWithOnlyAnswer;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+
+    @Cacheable(cacheNames = "answers", key = "'answers'.concat(#id)")
     Optional<ExerciseWithOnlyAnswer> findProjectedById(Long id);
 }
