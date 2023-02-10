5657bd7184ec6d8571fb44b91f5ab8e92acab036
xpakx <55586470+xpakx@users.noreply.github.com>
2023-02-10 11:49:36 +0100

    feat(alphabet): validate answer request

--- alphabet/src/main/java/io/github/xpakx/alingo/game/GameController.java
+++ alphabet/src/main/java/io/github/xpakx/alingo/game/GameController.java
@@ -2,6 +2,7 @@ package io.github.xpakx.alingo.game;
 
 import io.github.xpakx.alingo.game.dto.AnswerRequest;
 import io.github.xpakx.alingo.game.dto.AnswerResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +14,7 @@ public class GameController {
     private final GameService service;
 
     @PostMapping("/exercise/{exerciseId}")
-    public ResponseEntity<AnswerResponse> answer(@RequestBody AnswerRequest request, @PathVariable Long exerciseId) {
+    public ResponseEntity<AnswerResponse> answer(@Valid @RequestBody AnswerRequest request, @PathVariable Long exerciseId) {
         return new ResponseEntity<>(
                 service.checkAnswer(exerciseId, request),
                 HttpStatus.OK
--- alphabet/src/main/java/io/github/xpakx/alingo/game/dto/AnswerRequest.java
+++ alphabet/src/main/java/io/github/xpakx/alingo/game/dto/AnswerRequest.java
@@ -1,10 +1,12 @@
 package io.github.xpakx.alingo.game.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class AnswerRequest {
+    @NotBlank
     private String answer;
 }
