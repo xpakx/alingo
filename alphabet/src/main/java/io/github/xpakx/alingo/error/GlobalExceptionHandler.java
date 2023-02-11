3f92d87e12e62db2c66d510ebce442ded5789699
xpakx <55586470+xpakx@users.noreply.github.com>
2023-02-11 21:42:21 +0100

    feat(alphabet): add custom exception handler

---
+++ alphabet/src/main/java/io/github/xpakx/alingo/error/GlobalExceptionHandler.java
@@ -0,0 +1,41 @@
+package io.github.xpakx.alingo.error;
+
+import io.github.xpakx.alingo.error.dto.ErrorResponse;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler()
+    protected ResponseEntity<Object> handleException(RuntimeException ex, WebRequest request) {
+        HttpStatus status = getStatus(ex);
+        return handleExceptionInternal(
+                ex,
+                constructErrorBody(ex, status),
+                new HttpHeaders(),
+                status,
+                request
+        );
+    }
+
+    private ErrorResponse constructErrorBody(RuntimeException ex, HttpStatus status) {
+        ErrorResponse errorBody = new ErrorResponse();
+        errorBody.setMessage(ex.getMessage());
+        errorBody.setStatus(status.getReasonPhrase());
+        errorBody.setError(status.value());
+        return errorBody;
+    }
+
+    private HttpStatus getStatus(RuntimeException ex) {
+        ResponseStatus status = AnnotatedElementUtils.findMergedAnnotation(ex.getClass(), ResponseStatus.class);
+        return status != null ? status.code() : HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+}
