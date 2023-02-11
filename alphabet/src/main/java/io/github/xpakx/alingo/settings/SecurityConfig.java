e4ed668106c720b680f722fcc6b36e51fabc4339
xpakx <55586470+xpakx@users.noreply.github.com>
2023-02-11 22:13:45 +0100

    chore(alphabet): delete unnecessary beans

--- alphabet/src/main/java/io/github/xpakx/alingo/settings/SecurityConfig.java
+++ alphabet/src/main/java/io/github/xpakx/alingo/settings/SecurityConfig.java
@@ -5,8 +5,6 @@ import io.github.xpakx.alingo.security.JwtRequestFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,7 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.ExceptionTranslationFilter;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtRequestFilter jwtRequestFilter;
@@ -30,11 +28,5 @@ public class SecurityConfig {
                 .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
                 .addFilterAfter(jwtRequestFilter, ExceptionTranslationFilter.class);
         return http.build();
-    }
-
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
     }
 }
