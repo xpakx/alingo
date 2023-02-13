ebde16335415136dc7719009d8627ebd2b5181ce
xpakx <55586470+xpakx@users.noreply.github.com>
2023-02-13 13:25:14 +0100

    fix(alphabet): make sure request is of correct class before applying filter

--- alphabet/src/main/java/io/github/xpakx/alingo/security/JwtRequestFilter.java
+++ alphabet/src/main/java/io/github/xpakx/alingo/security/JwtRequestFilter.java
@@ -5,7 +5,6 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,12 +13,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -30,7 +27,9 @@ public class JwtRequestFilter extends GenericFilterBean {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
-        doFilterInternal((HttpServletRequest) request);
+        if(request instanceof HttpServletRequest) {
+            doFilterInternal((HttpServletRequest) request);
+        }
         filterChain.doFilter(request, response);
     }
 
