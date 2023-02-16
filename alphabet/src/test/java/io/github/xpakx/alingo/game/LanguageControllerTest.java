27c832f34bfddac4acb40a4636dccf19931809aa
xpakx <55586470+xpakx@users.noreply.github.com>
2023-02-16 22:51:36 +0100

    test(alphabet): test updating language

--- alphabet/src/test/java/io/github/xpakx/alingo/game/LanguageControllerTest.java
+++ alphabet/src/test/java/io/github/xpakx/alingo/game/LanguageControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.userdetails.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
@@ -155,5 +156,96 @@ class LanguageControllerTest {
                 .body("error", equalTo(BAD_REQUEST.value()))
                 .body("message", containsStringIgnoringCase("Validation failed"))
                 .body("errors", hasItem(both(containsStringIgnoringCase("name")).and(containsStringIgnoringCase("empty"))));
+    }
+
+    @Test
+    void shouldRespondWith401ToUpdateLanguageIfNotAuthenticated() {
+        when()
+                .put(baseUrl + "/language/{languageId}", 1L)
+        .then()
+                .statusCode(UNAUTHORIZED.value())
+                .body("error", equalTo(UNAUTHORIZED.value()))
+                .body("errors", nullValue());
+    }
+
+    @Test
+    void shouldRespondWith401ToUpdateLanguageIfTokenIsWrong() {
+        given()
+                .auth()
+                .oauth2("329432853295")
+                .contentType(ContentType.JSON)
+                .body(getLanguageRequest("language1"))
+        .when()
+                .put(baseUrl + "/language/{languageId}", 1L)
+        .then()
+                .statusCode(UNAUTHORIZED.value())
+                .body("error", equalTo(UNAUTHORIZED.value()))
+                .body("errors", nullValue());
+    }
+
+    @Test
+    void shouldRespondWith403ToUpdateLanguageIfUserIsNotModerator() {
+        given()
+                .auth()
+                .oauth2(tokenFor("user1"))
+                .contentType(ContentType.JSON)
+                .body(getLanguageRequest("language1"))
+        .when()
+                .put(baseUrl + "/language/{languageId}", 1L)
+        .then()
+                .statusCode(FORBIDDEN.value())
+                .body("error", equalTo(FORBIDDEN.value()))
+                .body("errors", nullValue());
+    }
+
+    @Test
+    void shouldRespondWith404ToUpdateLanguageIfLanguageDoesNotExist() {
+        given()
+                .auth()
+                .oauth2(tokenFor("user1", List.of(new SimpleGrantedAuthority("MODERATOR"))))
+                .contentType(ContentType.JSON)
+                .body(getLanguageRequest("language1"))
+        .when()
+                .put(baseUrl + "/language/{languageId}", 1L)
+        .then()
+                .statusCode(NOT_FOUND.value())
+                .body("error", equalTo(NOT_FOUND.value()))
+                .body("errors", nullValue());
+    }
+
+    @Test
+    void shouldUpdateLanguage() {
+        Long languageId = addLanguage("language1");
+        given()
+                .auth()
+                .oauth2(tokenFor("user1", List.of(new SimpleGrantedAuthority("MODERATOR"))))
+                .contentType(ContentType.JSON)
+                .body(getLanguageRequest("newName"))
+        .when()
+                .put(baseUrl + "/language/{languageId}", languageId)
+        .then()
+                .statusCode(OK.value())
+                .body("name", equalTo("newName"));
+    }
+
+    private Long addLanguage(String name) {
+        Language language = new Language();
+        language.setName(name);
+        return languageRepository.save(language).getId();
+    }
+
+    @Test
+    void shouldUpdateLanguageIndDb() {
+        Long languageId = addLanguage("language1");
+        given()
+                .auth()
+                .oauth2(tokenFor("user1", List.of(new SimpleGrantedAuthority("MODERATOR"))))
+                .contentType(ContentType.JSON)
+                .body(getLanguageRequest("newName"))
+        .when()
+                .put(baseUrl + "/language/{languageId}", languageId);
+        Optional<Language> language = languageRepository.findById(languageId);
+        assertTrue(language.isPresent());
+        assertThat(language.get(), hasProperty("name", equalTo("newLanguage")));
     }
 }
