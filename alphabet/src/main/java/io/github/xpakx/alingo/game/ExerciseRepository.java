1eec0fbc35810d7769b437477b76919d3d6e2c9c
xpakx <55586470+xpakx@users.noreply.github.com>
2023-02-23 20:52:27 +0100

    feat(alphabet); add repository methods for incrementing/decrementing order

--- alphabet/src/main/java/io/github/xpakx/alingo/game/ExerciseRepository.java
+++ alphabet/src/main/java/io/github/xpakx/alingo/game/ExerciseRepository.java
@@ -1,10 +1,12 @@
 package io.github.xpakx.alingo.game;
 
 import io.github.xpakx.alingo.game.dto.ExerciseWithOnlyAnswer;
+import jakarta.transaction.Transactional;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
@@ -16,4 +18,14 @@ public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
     Page<Exercise> findByCourseId(Long courseId, Pageable pageable);
     @Query("SELECT coalesce(max(ex.order), 0) FROM Exercise ex WHERE ex.course.id = :courseId")
     Integer getMaxOrderByCourseId(Long courseId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Exercise ex SET ex.order = ex.order + 1 WHERE ex.course.id = :courseId AND ex.order > :orderStart AND ex.order < :orderEnd")
+    void incrementOrderBetween(Long courseId, Integer orderStart, Integer orderEnd);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Exercise ex SET ex.order = ex.order - 1 WHERE ex.course.id = :courseId AND ex.order > :orderStart AND ex.order < :orderEnd")
+    void decrementOrderBetween(Long courseId, Integer orderStart, Integer orderEnd);
 }
