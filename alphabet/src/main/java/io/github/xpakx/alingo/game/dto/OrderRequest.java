a20f616efe75f28fc207f06dc8930d2d3b973bdc
xpakx <55586470+xpakx@users.noreply.github.com>
2023-02-24 11:32:28 +0100

    feat(alphabet): add better validation for order request

--- alphabet/src/main/java/io/github/xpakx/alingo/game/dto/OrderRequest.java
+++ alphabet/src/main/java/io/github/xpakx/alingo/game/dto/OrderRequest.java
@@ -1,6 +1,7 @@
 package io.github.xpakx.alingo.game.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 
-public record OrderRequest(@NotNull Integer newOrder) {
+public record OrderRequest(@NotNull(message = "Order must be provided") @PositiveOrZero(message = "Order cannot be negative") Integer newOrder) {
 }
