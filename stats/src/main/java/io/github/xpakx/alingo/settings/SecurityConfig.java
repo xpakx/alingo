c3f190539134f8b351963832b65b15fc22633025
xpakx <55586470+xpakx@users.noreply.github.com>
2023-02-28 14:14:50 +0100

    chore(stats): configure securuty

---
+++ stats/src/main/java/io/github/xpakx/alingo/settings/SecurityConfig.java
@@ -0,0 +1,34 @@
+package io.github.xpakx.alingo.settings;
+
+import io.github.xpakx.alingo.security.JwtAuthenticationEntryPoint;
+import io.github.xpakx.alingo.security.JwtRequestFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtRequestFilter jwtRequestFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
+                .sessionManagement(
+                        sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .addFilterAfter(jwtRequestFilter, ExceptionTranslationFilter.class);
+        return http.build();
+    }
+}
