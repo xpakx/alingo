747145b1dea29a32db69dbbb48ffa1ea809a4c76
xpakx <55586470+xpakx@users.noreply.github.com>
2023-02-28 14:21:59 +0100

    feat(stats): save new guesses to database

--- stats/src/main/java/io/github/xpakx/alingo/stats/StatService.java
+++ stats/src/main/java/io/github/xpakx/alingo/stats/StatService.java
@@ -1,11 +1,23 @@
 package io.github.xpakx.alingo.stats;
 
 import io.github.xpakx.alingo.stats.event.GuessEvent;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class StatService {
-    public void newGuess(GuessEvent event) {
+    private final GuessRepository repository;
 
+    public void newGuess(GuessEvent event) {
+        Guess guess = new Guess();
+        guess.setCorrect(event.isCorrect());
+        guess.setCourseId(event.getCourseId());
+        guess.setCourseName(event.getCourseName());
+        guess.setExerciseId(event.getExerciseId());
+        guess.setLanguage(event.getLanguage());
+        guess.setLetter(event.getLetter());
+        guess.setUsername(event.getUsername());
+        repository.save(guess);
     }
 }
