d52a389161e9abcb4fc1a3f65116f212611ade07
xpakx <55586470+xpakx@users.noreply.github.com>
2023-02-28 14:18:03 +0100

    feat(stats): let users view only their own stats

--- stats/src/main/java/io/github/xpakx/alingo/stats/GuessController.java
+++ stats/src/main/java/io/github/xpakx/alingo/stats/GuessController.java
@@ -2,6 +2,7 @@ package io.github.xpakx.alingo.stats;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,6 +12,7 @@ public class GuessController {
 
     @GetMapping("/stats/{username}/alphabet")
     @ResponseBody
+    @PreAuthorize("#username == authentication.principal.username")
     public Page<Guess> getGuesses(Integer page, Integer amount, @PathVariable String username) {
         return service.getGuesses(username, page-1, amount);
     }
