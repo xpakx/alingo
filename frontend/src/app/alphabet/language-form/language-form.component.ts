26a1b35e3152494abfd77a6b675b15c93fd1dc6c
xpakx <55586470+xpakx@users.noreply.github.com>
2023-03-03 16:39:38 +0100

    feat(frontend): let moderator update langauge

--- frontend/src/app/alphabet/language-form/language-form.component.ts
+++ frontend/src/app/alphabet/language-form/language-form.component.ts
@@ -1,5 +1,5 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlphabetModerationService } from '../alphabet-moderation.service';
 import { LanguageDetails } from '../dto/language-details';
@@ -10,21 +10,44 @@ import { LanguageForm } from '../form/language-form';
   templateUrl: './language-form.component.html',
   styleUrls: ['./language-form.component.css']
 })
-export class LanguageFormComponent implements OnInit {
+export class LanguageFormComponent implements OnInit{
   form: FormGroup<LanguageForm>;
   isError: boolean = false;
   errorMsg: String = "";
+  @Input("language") language?: LanguageDetails;
 
   constructor(private fb: FormBuilder, private modService: AlphabetModerationService) {
     this.form = this.fb.group({
       name: [new String(""), [Validators.required, Validators.minLength(1)]]
     });
-   }
+  }
 
   ngOnInit(): void {
+    if(this.language) {
+      this.form.setValue({name: this.language.name});
+    }
   }
 
   onSubmit(): void {
+    if(this.language) {
+      this.updateLanguage(this.language.id);
+    } else {
+      this.createLanguage();
+    }
+  }
+
+  updateLanguage(id: number): void {
+    if(this.form.valid) {
+      this.modService.updateLanguage(id, { 
+        name: this.form.controls.name.value ? this.form.controls.name.value : ""
+      }).subscribe({
+        next: (response: LanguageDetails) => this.onCreation(response),
+        error: (error: HttpErrorResponse) => this.onError(error)
+      });
+    }
+  }
+
+  createLanguage(): void {
     if(this.form.valid) {
       this.modService.createLanguage({ 
         name: this.form.controls.name.value ? this.form.controls.name.value : ""
@@ -43,6 +66,4 @@ export class LanguageFormComponent implements OnInit {
     this.isError = true;
     this.errorMsg = error.error.message;
   }
-
-
 }
