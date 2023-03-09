172c0b0bfa9e665dcd9b42683dc3602da90e9d1e
xpakx <55586470+xpakx@users.noreply.github.com>
2023-03-09 12:15:03 +0100

    fix(alphabet): add validatio to method for getting languages

--- alphabet/src/main/java/io/github/xpakx/alingo/game/LanguageController.java
+++ alphabet/src/main/java/io/github/xpakx/alingo/game/LanguageController.java
@@ -3,16 +3,21 @@ package io.github.xpakx.alingo.game;
 import io.github.xpakx.alingo.game.dto.CourseData;
 import io.github.xpakx.alingo.game.dto.LanguageRequest;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Validated
 @RequestMapping("/language")
 public class LanguageController {
     private final LanguageService service;
@@ -43,14 +48,17 @@ public class LanguageController {
     @GetMapping("/{languageId}/course")
     @Secured("MODERATOR")
     @ResponseBody
-    public List<CourseData> getCourses(@RequestParam Integer page, @RequestParam Integer amount, @PathVariable Long languageId) {
+    public List<CourseData> getCourses(@RequestParam  @Min(value = 1, message = "Page must be positive") @NotNull(message = "Page cannot be null") Integer page,
+                                       @RequestParam @NotNull @Min(value = 1, message = "Amount must be between 1 and 20") @Max(value = 20, message = "Amount must be between 1 and 20") Integer amount,
+                                       @PathVariable Long languageId) {
         return service.getCourses(languageId, page, amount);
     }
 
     @GetMapping
     @Secured("MODERATOR")
     @ResponseBody
-    public List<Language> getLanguages(@RequestParam Integer page, @RequestParam Integer amount) {
+    public List<Language> getLanguages(@RequestParam @Min(value = 1, message = "Page must be positive") @NotNull(message = "Page cannot be null") Integer page,
+                                       @RequestParam @NotNull @Min(value = 1, message = "Amount must be between 1 and 20") @Max(value = 20, message = "Amount must be between 1 and 20") Integer amount) {
         return service.getLanguages(page, amount);
     }
 }
