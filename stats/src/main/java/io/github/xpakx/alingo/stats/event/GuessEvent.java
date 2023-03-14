4fd652432893ce0e786fcf004919d67da48b4d89
xpakx <55586470+xpakx@users.noreply.github.com>
2023-03-14 21:10:15 +0100

    fix(alphabet): fix problem with serializing date for broker

--- stats/src/main/java/io/github/xpakx/alingo/stats/event/GuessEvent.java
+++ stats/src/main/java/io/github/xpakx/alingo/stats/event/GuessEvent.java
@@ -1,5 +1,10 @@
 package io.github.xpakx.alingo.stats.event;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,5 +20,8 @@ public class GuessEvent {
     private Long courseId;
     private String courseName;
     private String language;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime time;
 }
