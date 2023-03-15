4a2b11d43a6a85e280a7f40184bc24401a904d62
xpakx <55586470+xpakx@users.noreply.github.com>
2023-03-15 13:26:22 +0100

    chore(sound): remove dependencies

--- sound/src/main/java/io/github/xpakx/alingo/SoundController.java
+++ sound/src/main/java/io/github/xpakx/alingo/SoundController.java
@@ -13,12 +13,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class SoundController {
     private final SoundService service;
 
-    @GetMapping("/files/{filename:.+}")
-public ResponseEntity<Resource> getFile(@PathVariable String filename) {
-    Resource file = service.getSound(filename);
-    return ResponseEntity.ok().header(
-            HttpHeaders.CONTENT_DISPOSITION,
-            "attachment; filename=\"" + file.getFilename() + "\""
-    ).body(file);
-}
+    @GetMapping("/sound/{filename:.+}")
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        System.out.println(filename);
+        Resource file = service.getSound(filename);
+        return ResponseEntity.ok().header(
+                HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\""
+        ).body(file);
+    }
 }
