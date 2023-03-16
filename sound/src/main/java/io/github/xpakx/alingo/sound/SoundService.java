ea9f664987a13bc130d917cad91e664a76129db9
xpakx <55586470+xpakx@users.noreply.github.com>
2023-03-16 18:12:20 +0100

    feat(sound): create directory for sounds at start up if necessarily

--- sound/src/main/java/io/github/xpakx/alingo/sound/SoundService.java
+++ sound/src/main/java/io/github/xpakx/alingo/sound/SoundService.java
@@ -2,11 +2,15 @@ package io.github.xpakx.alingo.sound;
 
 import io.github.xpakx.alingo.sound.dto.UploadResponse;
 import io.github.xpakx.alingo.sound.error.FileException;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,6 +23,20 @@ import java.util.Optional;
 @Service
 public class SoundService {
     private final Path root = Paths.get("sounds");
+    Logger logger = LoggerFactory.getLogger(SoundService.class);
+
+    @PostConstruct
+    public void init() {
+        try {
+            if(!Files.exists(root)) {
+                Files.createDirectory(root);
+            } else if(!Files.isDirectory(root)) {
+                logger.error("Cannot create directory for file upload!");
+            }
+        } catch (IOException e) {
+            logger.error("Cannot create directory for file upload!");
+        }
+    }
 
     public Resource getSound(String filename) {
         try {
