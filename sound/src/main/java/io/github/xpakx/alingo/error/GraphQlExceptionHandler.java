a59ebf4125691cec8da484977508421852c00dcc
xpakx <55586470+xpakx@users.noreply.github.com>
2023-04-08 19:15:09 +0200

    feat(sound): add better error handling

---
+++ sound/src/main/java/io/github/xpakx/alingo/error/GraphQlExceptionHandler.java
@@ -0,0 +1,76 @@
+package io.github.xpakx.alingo.error;
+
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+import graphql.schema.DataFetchingEnvironment;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.graphql.execution.ErrorType;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.Objects;
+
+@Component
+public class GraphQlExceptionHandler  extends DataFetcherExceptionResolverAdapter {
+    @Override
+    protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+        if(ex instanceof ConstraintViolationException) {
+            return GraphqlErrorBuilder.newError()
+                    .errorType(ErrorType.BAD_REQUEST)
+                    .message(ex.getMessage())
+                    .path(env.getExecutionStepInfo().getPath())
+                    .location(env.getField().getSourceLocation())
+                    .build();
+        }
+        if(ex instanceof RuntimeException) {
+            return GraphqlErrorBuilder.newError()
+                    .errorType(getStatus(ex))
+                    .message(ex.getMessage())
+                    .path(env.getExecutionStepInfo().getPath())
+                    .location(env.getField().getSourceLocation())
+                    .build();
+        }
+        return null;
+    }
+
+    @Override
+    protected List<GraphQLError> resolveToMultipleErrors(Throwable ex, DataFetchingEnvironment env) {
+        if(ex instanceof ConstraintViolationException) {
+            return ((ConstraintViolationException) ex).getConstraintViolations()
+                    .stream()
+                    .map(ConstraintViolation::getMessage)
+                    .filter(Objects::nonNull)
+                    .map(s -> toGraphQLError(s, env))
+                    .toList();
+        }
+        return super.resolveToMultipleErrors(ex, env);
+    }
+
+    private GraphQLError toGraphQLError(String s, DataFetchingEnvironment env) {
+        return GraphqlErrorBuilder.newError()
+                .errorType(ErrorType.BAD_REQUEST)
+                .message(s)
+                .path(env.getExecutionStepInfo().getPath())
+                .location(env.getField().getSourceLocation())
+                .build();
+    }
+
+    private ErrorType getStatus(Throwable ex) {
+        ResponseStatus status = AnnotatedElementUtils.findMergedAnnotation(ex.getClass(), ResponseStatus.class);
+        if(status == null) {
+            return ErrorType.INTERNAL_ERROR;
+        }
+        if(status.code() == HttpStatus.NOT_FOUND) {
+            return ErrorType.NOT_FOUND;
+        }
+        if(status.code() == HttpStatus.BAD_REQUEST) {
+            return ErrorType.BAD_REQUEST;
+        }
+        return ErrorType.INTERNAL_ERROR;
+    }
+}
--- sound/src/main/java/io/github/xpakx/alingo/sound/SoundController.java
+++ sound/src/main/java/io/github/xpakx/alingo/sound/SoundController.java
@@ -2,17 +2,21 @@ package io.github.xpakx.alingo.sound;
 
 import io.github.xpakx.alingo.sound.dto.FilesResponse;
 import io.github.xpakx.alingo.sound.dto.UploadResponse;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequiredArgsConstructor
+@Validated
 public class SoundController {
     private final SoundService service;
 
@@ -35,7 +39,7 @@ public class SoundController {
     @GetMapping("/sound/list")
     @Secured("MODERATOR")
     @ResponseBody
-    public FilesResponse getListOfFiles(@RequestParam Integer page) {
+    public FilesResponse getListOfFiles(@Min(value = 1, message = "Page must be positive") @NotNull(message = "Page cannot be null") @RequestParam Integer page) {
         return service.getFileNames(page-1);
     }
 }
